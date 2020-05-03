b4f1972effd3226edb6b81374f0e19a57987ee56
Avtar Prajapati <prajaptiavtar@gmail.com>
2020-05-03 20:27:10 +0530

    add reducer

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,20 +1,42 @@
 import _ from 'lodash';
 import {
+  FETCH_SEARCH_VALUE,
+  FETCH_MOVIE_DETAILS,
   FETCH_POPULAR_MOVIE,
   FETCH_TRENDING_MOVIE,
-  FETCH_TOP_RATED
+  FETCH_TOP_RATED_MOVIE,
+  FETCH_UPCOMING_MOVIE,
+  FETCH_NOW_PLAYING_MOVIE
 } from '../actions/typeConfig';
 
 const INITIAL_VALUE = {
+  searchResult: {},
+  movieDetail: {},
   popular: {},
   trending: {},
   topRated: {},
   upcoming: {},
-  movieDetail: {}
+  nowPlaying: {}
 };
 
 export default (state = INITIAL_VALUE, action) => {
   switch (action.type) {
+    case FETCH_SEARCH_VALUE:
+      return {
+        ...state,
+        searchResult: {
+          ...state.searchResult,
+          ..._.mapKeys(action.payload, 'id')
+        }
+      };
+    case FETCH_MOVIE_DETAILS:
+      return {
+        ...state,
+        movieDetail: {
+          ...state.movieDetail,
+          ..._.mapKeys(action.payload, 'id')
+        }
+      };
     case FETCH_POPULAR_MOVIE:
       return {
         ...state,
@@ -25,10 +47,20 @@ export default (state = INITIAL_VALUE, action) => {
         ...state,
         trending: { ...state.trending, ..._.mapKeys(action.payload, 'id') }
       };
-    case FETCH_TOP_RATED:
+    case FETCH_TOP_RATED_MOVIE:
       return {
         ...state,
         topRated: { ...state.topRated, ..._.mapKeys(action.payload, 'id') }
+      };
+    case FETCH_UPCOMING_MOVIE:
+      return {
+        ...state,
+        upcoming: { ...state.upcoming, ..._.mapKeys(action.payload, 'id') }
+      };
+    case FETCH_NOW_PLAYING_MOVIE:
+      return {
+        ...state,
+        nowPlaying: { ...state.nowPlaying, ..._.mapKeys(action.payload, 'id') }
       };
     default:
       return { ...state };
--- src/reducers/tvReducer.js
+++ src/reducers/tvReducer.js
@@ -1,20 +1,69 @@
 import _ from 'lodash';
-import { FETCH_POPULAR_TV, FETCH_TRENDING_TV } from '../actions/typeConfig';
+import {
+  FETCH_TV_DETAILS,
+  FETCH_POPULAR_TV,
+  FETCH_TRENDING_TV,
+  FETCH_TOP_RATED_TV,
+  FETCH_AIRING_TODAY__TV,
+  FETCH_ON_THE_AIR__TV
+} from '../actions/typeConfig';
 
 const INITIAL_VALUE = {
+  searchResult: {},
+  tvDetail: {},
   popular: {},
   trending: {},
   topRated: {},
-  upcoming: {},
-  movieDetail: {}
+  airingToday: {},
+  onTheAir: {}
 };
 
 export default (state = INITIAL_VALUE, action) => {
   switch (action.type) {
+    case FETCH_SEARCH_VALUE:
+      return {
+        ...state,
+        searchResult: {
+          ...state.searchResult,
+          ..._.mapKeys(action.payload, 'id')
+        }
+      };
+    case FETCH_TV_DETAILS:
+      return {
+        ...state,
+        tvDetail: {
+          ...state.tvDetail,
+          ..._.mapKeys(action.payload, 'id')
+        }
+      };
     case FETCH_POPULAR_TV:
-      return { ...state, popular: _.mapKeys(action.payload, 'id') };
+      return {
+        ...state,
+        popular: { ...state.popular, ..._.mapKeys(action.payload, 'id') }
+      };
     case FETCH_TRENDING_TV:
-      return { ...state, trending: _.mapKeys(action.payload, 'id') };
+      return {
+        ...state,
+        trending: { ...state.trending, ..._.mapKeys(action.payload, 'id') }
+      };
+    case FETCH_TOP_RATED_TV:
+      return {
+        ...state,
+        topRated: { ...state.topRated, ..._.mapKeys(action.payload, 'id') }
+      };
+    case FETCH_AIRING_TODAY__TV:
+      return {
+        ...state,
+        airingToday: {
+          ...state.airingToday,
+          ..._.mapKeys(action.payload, 'id')
+        }
+      };
+    case FETCH_ON_THE_AIR__TV:
+      return {
+        ...state,
+        onTheAir: { ...state.onTheAir, ..._.mapKeys(action.payload, 'id') }
+      };
     default:
       return { ...state };
   }
