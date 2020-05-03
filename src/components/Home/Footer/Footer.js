b40cdf81209cf0d498c8ad4b4755851fa4b75db0
Avtar Prajapati <prajaptiavtar@gmail.com>
2020-05-03 10:32:09 +0530

    footer component add

---
+++ src/components/Home/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <p>&copy; Avtar Prajapati 2020</p>
+    </footer>
+  );
+};
+
+export default Footer;
