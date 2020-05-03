888d00d0f6cf30972d9cc1db3c716c5fcc7a4d27
Avtar Prajapati <prajaptiavtar@gmail.com>
2020-05-03 12:42:08 +0530

    correct route path

--- src/components/App.js
+++ src/components/App.js
@@ -26,14 +26,10 @@ class App extends Component {
             <Navbar />
             <Switch>
               <Route path="/" exact component={Home} />
-              <Route path="/movies/popular" exact component={PopularMovies} />
-              <Route path="/movies/toprated" exact component={TopRatedMovies} />
-              <Route path="/movies/upcoming" exact component={UpcomingMovies} />
-              <Route
-                path="/movies/nowplaying"
-                exact
-                component={NowPlayMovies}
-              />
+              <Route path="/movie/popular" exact component={PopularMovies} />
+              <Route path="/movie/toprated" exact component={TopRatedMovies} />
+              <Route path="/movie/upcoming" exact component={UpcomingMovies} />
+              <Route path="/movie/nowplaying" exact component={NowPlayMovies} />
               <Route path="/tv/popular" exact component={PopularTV} />
               <Route path="/tv/toprated" exact component={TopRatedTV} />
               <Route path="/movie/:id" exact component={MovieDetails} />
