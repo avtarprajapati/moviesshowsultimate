17f0e9df96decd6d8d5cf4370614d9e48c9600b6
Avtar Prajapati <prajaptiavtar@gmail.com>
2020-05-03 18:09:51 +0530

    movie top rated

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 import themoviedb from '../apis/themoviedb';
 import {
   FETCH_POPULAR_MOVIE,
-  FETCH_POPULAR_TV,
   FETCH_TRENDING_MOVIE,
+  FETCH_TOP_RATED,
+  FETCH_POPULAR_TV,
   FETCH_TRENDING_TV
 } from './typeConfig';
 
@@ -38,28 +39,6 @@ export const fetchPopularMovies = (page = 1) => async (dispatch, getState) => {
   });
 };
 
-export const fetchPopularTV = (page = 1) => async (dispatch, getState) => {
-  const response = await url(`/tv/popular`, page);
-
-  const popularTV = response.data.results.map((tv) => {
-    return {
-      id: tv.id,
-      title: tv.name,
-      backdrop_path: tv.backdrop_path,
-      poster_path: tv.poster_path,
-      overview: tv.overview,
-      release_date: tv.first_air_date,
-      score: tv.vote_average,
-      type: 'tv'
-    };
-  });
-
-  dispatch({
-    type: FETCH_POPULAR_TV,
-    payload: popularTV
-  });
-};
-
 export const fetchTrendingMovie = (page = 1) => async (dispatch, getState) => {
   const response = await url(`/trending/movie/day`, page);
 
@@ -79,6 +58,50 @@ export const fetchTrendingMovie = (page = 1) => async (dispatch, getState) => {
   dispatch({
     type: FETCH_TRENDING_MOVIE,
     payload: trendingMovie
+  });
+};
+
+export const fetchTopRatedMovies = (page = 1) => async (dispatch, getState) => {
+  const response = await url(`/movie/top_rated`, page);
+
+  const popularMovie = response.data.results.map((movie) => {
+    return {
+      id: movie.id,
+      title: movie.title,
+      backdrop_path: movie.backdrop_path,
+      poster_path: movie.poster_path,
+      overview: movie.overview,
+      release_date: movie.release_date,
+      score: movie.vote_average,
+      type: 'movie'
+    };
+  });
+
+  dispatch({
+    type: FETCH_TOP_RATED,
+    payload: popularMovie
+  });
+};
+
+export const fetchPopularTV = (page = 1) => async (dispatch, getState) => {
+  const response = await url(`/tv/popular`, page);
+
+  const popularTV = response.data.results.map((tv) => {
+    return {
+      id: tv.id,
+      title: tv.name,
+      backdrop_path: tv.backdrop_path,
+      poster_path: tv.poster_path,
+      overview: tv.overview,
+      release_date: tv.first_air_date,
+      score: tv.vote_average,
+      type: 'tv'
+    };
+  });
+
+  dispatch({
+    type: FETCH_POPULAR_TV,
+    payload: popularTV
   });
 };
 
--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import {
   FETCH_POPULAR_MOVIE,
-  FETCH_TRENDING_MOVIE
+  FETCH_TRENDING_MOVIE,
+  FETCH_TOP_RATED
 } from '../actions/typeConfig';
 
 const INITIAL_VALUE = {
@@ -23,6 +24,11 @@ export default (state = INITIAL_VALUE, action) => {
       return {
         ...state,
         trending: { ...state.trending, ..._.mapKeys(action.payload, 'id') }
+      };
+    case FETCH_TOP_RATED:
+      return {
+        ...state,
+        topRated: { ...state.topRated, ..._.mapKeys(action.payload, 'id') }
       };
     default:
       return { ...state };
