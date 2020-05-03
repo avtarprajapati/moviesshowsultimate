d31ea4452865bb5c6bf5ae465f9e77d50ebf751a
Avtar Prajapati <prajaptiavtar@gmail.com>
2020-05-03 18:08:06 +0530

    add top rated movie page

--- src/components/movies/TopRated.js
+++ src/components/movies/TopRated.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchTopRatedMovies } from '../../actions';
+
+import Card from '../reusable/Card';
+
+import './MovieStyle.scss';
 
 class TopRated extends Component {
+  state = { page: 1 };
+
+  componentDidMount() {
+    if (!this.props.topRatedMovie.length)
+      this.props.fetchTopRatedMovies(this.state.page);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.page !== this.state.page) {
+      this.props.fetchTopRatedMovies(this.state.page);
+    }
+  }
+
+  renderCard(movies) {
+    return movies.map((movie) => <Card key={movie.id} show={movie} />);
+  }
+
+  onInputClick = () => {
+    this.setState((prevState) => ({ page: prevState.page + 1 }));
+  };
+
   render() {
-    return <div>TopRated</div>;
+    return (
+      <section className="section">
+        <h2 className="heading2">Top Rated Movies</h2>
+        <div className="wrap">{this.renderCard(this.props.topRatedMovie)}</div>
+        <button className="btn__load" onClick={this.onInputClick}>
+          Load More
+        </button>
+      </section>
+    );
   }
 }
 
-export default TopRated;
+function mapStateToProps(state) {
+  return {
+    topRatedMovie: Object.values(state.movie.topRated)
+  };
+}
+
+export default connect(mapStateToProps, { fetchTopRatedMovies })(TopRated);
